f11dfae8922c8549840b46fc13a09465690c5709
Jamie Hegarty <Jamiehegarty@hotmail.com>
2016-02-06 22:09:05 +0000

    Refactored reading from file
    This update was to refactor the functionality of reading from the
    dictionary file, this time when the file is read in each word is sorted
    into the correct list (depending on size) and any of an unusable size
    are discarded

--- FalloutTerminalClone/FalloutTerminalClone.cpp
+++ FalloutTerminalClone/FalloutTerminalClone.cpp
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 #include <fstream>
 #include <iostream>
-
 #include <string>
 #include <vector>
 
-const std::string DICTIONARY_FILE = "dictionary.txt";
-std::vector<std::string> wordList;
+/*
+	The minigame features four levels of dificulty with varied number and length of words
+		Novie - 5 Letters - 6 Words
+		Advanced - 7 Letters - 8 Words
+		Expert - 9 Letters - 10 Words
+		Master - 11 Letters - 12 Words
+*/
 
+//Each dificulty has it's own set of words
+std::vector<std::string> noviceWords, advancedWords, expertWords, masterWords;
+
+const std::string DICTIONARY_FILE = "dictionary.txt";
+
+/* 
+	The loadDictionary function reads in words from a specified text file
+	and then arranges them into the correct category (explained above)
+*/
 static bool loadDictionary() {
 
 	std::ifstream fileStream(DICTIONARY_FILE); //Open dictionary file using input file stream
@@ -19,13 +32,39 @@ static bool loadDictionary() {
 	
 		//Read each line (word) from the dictionary
 		while (std::getline(fileStream, line)) {
-			wordList.push_back(line);
-			std::cout << line << std::endl;
+
+			//Switch statement to sort our words into the corresponding vector based on size
+			switch (line.length()) {
+			case 5:
+				noviceWords.push_back(line);
+				break;
+			case 7:
+				advancedWords.push_back(line);
+				break;
+			case 9:
+				expertWords.push_back(line);
+				break;
+			case 11:
+				masterWords.push_back(line);
+				break;
+			default:
+				//Do nothing if the word is not of the correct length	
+				break;
+			}
 		}
 
-		fileStream.close();
+		fileStream.close(); //Close file stream when we are finished with it
 
-		return true;
+		//Check that each word category has been populated correctly
+		if (noviceWords.size() && advancedWords.size() && expertWords.size() && masterWords.size()) {
+			//This print statement is for debugging only, to be removed at later itterations
+			std::cout << "No. Novice Words: " << noviceWords.size() << "\nNo. Advanced Words: " << advancedWords.size() << "\nNo. Expert Words: " <<  expertWords.size() << "\nNo. Master Words: " << masterWords.size() << std::endl;
+			return true;
+
+		} else {
+			std::cout << "Error reading from dictionary, word lists couldn't be populated!" << std::endl;
+			return false;
+		}
 
 	} else {
 
@@ -38,9 +77,8 @@ static bool loadDictionary() {
 int main() {
 
 	if (loadDictionary()) {
-		std::cout << "Dictionary Loaded" << std::endl;
+			std::cout << "Dictionary Loaded" << std::endl;
 	}
-
 
     return 0;
 }
